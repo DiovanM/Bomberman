4164225cf483432061e339ce440c13c5d10005f3
DiovanM <diovanmarques@hotmail.com>
2017-07-31 05:06:02 -0300

    explosions destroying blocks

--- Bomberman/Assets/Scripts/DestroyMe.cs
+++ Bomberman/Assets/Scripts/DestroyMe.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyMe : MonoBehaviour {
+	
 
-	public void destroyMe(){
-		Destroy (gameObject);
-	}
+
 }
--- Bomberman/Assets/Scripts/Explosion.cs
+++ Bomberman/Assets/Scripts/Explosion.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class Explosion : MonoBehaviour {
 
 
-	void Start () {				
+	void Start () {	
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){				
+		//Debug.Log(other.gameObject.name);
+		if (other.gameObject.CompareTag("Destroyable")) {
+			Destroy (other.gameObject);
+		}
+
 	}
 
 	public void DestroyMe(){
