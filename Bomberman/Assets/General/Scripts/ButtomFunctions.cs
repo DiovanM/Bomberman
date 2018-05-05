9dbc34274dfbaaf4c924e0fc3e458e89c9de9ec5
Diovan <diovanmarques@gmail.com>
2018-05-05 06:05:35 -0300

    Added scoreboard visuals and access buttons in the main menu

--- Bomberman/Assets/General/Scripts/ButtomFunctions.cs
+++ Bomberman/Assets/General/Scripts/ButtomFunctions.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ButtomFunctions : MonoBehaviour {
 
-    public GameObject startButton, levelOne, levelTwo, boss;
+    public GameObject startButton, exitButton, scoreboardButton, returnMenu, levelOne, levelTwo, boss, scoreboard;
 
 	public void Load(int sceneNumber){
 		Time.timeScale = 1;
@@ -16,11 +16,39 @@ public class ButtomFunctions : MonoBehaviour {
 		Application.Quit();
 	}
 
+    public void Scores()
+    {
+        startButton.SetActive(false);
+        exitButton.SetActive(false);
+        scoreboardButton.SetActive(false);
+        scoreboard.SetActive(true);
+    }
+
+    public void Return()
+    {
+        startButton.SetActive(true);
+        exitButton.SetActive(true);
+        scoreboardButton.SetActive(true);
+        scoreboard.SetActive(false);
+    }
+
     public void MenuButtons() {
         startButton.SetActive(false);
         levelOne.SetActive(true);
         levelTwo.SetActive(true);
         boss.SetActive(true);
+        scoreboardButton.SetActive(false);
+        returnMenu.SetActive(true);
+    }
+
+    public void ReturnMenu()
+    {
+        startButton.SetActive(true);
+        levelOne.SetActive(false);
+        levelTwo.SetActive(false);
+        boss.SetActive(false);
+        scoreboardButton.SetActive(true);
+        returnMenu.SetActive(false);
     }
 
 }
